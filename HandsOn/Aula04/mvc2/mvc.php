d1b627bc8a101decebad88cb36f7d1edd42ef77e
daniel.ribeiro@4linux.com.br <daniel.ribeiro@4linux.com.br>
2016-11-05 17:34:52 -0200

    Aula03 e Aula04

---
+++ HandsOn/Aula04/mvc2/mvc.php
@@ -0,0 +1,20 @@
+<?php
+	require('Model.php');
+	require('View.php');
+	require('Controller.php');
+
+	$model = new Model();
+	$controller = new Controller($model);
+	$view = new View($model,$controller);
+
+	if(isset($_GET['action']) AND isset($_GET['id'])){
+		echo $controller->{$_GET['action']}($_POST,$_GET['id']);
+	}elseif(isset($_GET['action'])){
+		echo $controller->{$_GET['action']}($_POST);
+	}
+
+	if(isset($_GET['id'])){
+		echo $view->montarForm($_GET['id']);
+	} else {
+		echo $view->listarUsuarios();
+	}
